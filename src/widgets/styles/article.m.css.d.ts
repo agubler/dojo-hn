385c295d1920d2eae53962687c82020e389b35c5
Anthony Gubler <agubler@sitepen.com>
2017-09-23 21:54:57 +0100

    gh-pages

--- src/widgets/styles/article.m.css.d.ts
+++
@@ -1,7 +0,0 @@
-export const root: string;
-export const pageNumber: string;
-export const post: string;
-export const title: string;
-export const details: string;
-export const link: string;
-export const comments: string;
