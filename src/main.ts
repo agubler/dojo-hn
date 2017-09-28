337392c28f77f79f37594bef4d4bb4281770078f
Anthony Gubler <agubler@sitepen.com>
2017-09-28 15:11:05 +0100

    latest build

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,12 @@ import { registerRouterInjector } from '@dojo/routing/RouterInjector';
 import { Context } from './Context';
 
 import { App } from './widgets/App';
-import register from './registerServiceWorker';
-
-import load from '@dojo/core/load';
-
-declare const require: any;
 
 const registry = new Registry();
 registry.defineInjector('state', new Context({}));
-registry.define('comments', () => {
-	return load(require, './widgets/Comments').then(([someWidget]) => someWidget.Comments);
+registry.define('comments', async () => {
+	const widget = await import ('./widgets/Comments');
+	return widget.Comments;
 });
 
 const routingConfig = [
@@ -36,11 +32,8 @@ const routingConfig = [
 	}
 ];
 const router = registerRouterInjector(routingConfig, registry);
-
 const Projector = ProjectorMixin(App);
 const projector = new Projector();
 projector.setProperties({ registry });
 projector.append();
 router.start();
-
-register();
